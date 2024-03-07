8780237aa6e7759f4684da091c3a0801eb5f947a
ibrahim0891 <nijhum0891@gmail.com>
2024-03-07 19:47:22 +0600

    f

--- src/Controller/PostAdder.jsx
+++ src/Controller/PostAdder.jsx
@@ -2,8 +2,7 @@
 import { createUniqeKey , updateDataInDb } from "../Model/Database";
 
 export function PostAdder(postData) {
-
-    const user = JSON.parse(sessionStorage.getItem('user')); 
+ 
 
     createUniqeKey('/posts').then((key) => {
         const updates = {};
