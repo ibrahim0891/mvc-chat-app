c09cb16eb9ca518aca2e0b38960d2c0b6060aa43
ibrahim0891 <nijhum0891@gmail.com>
2024-03-16 16:48:47 +0600

    updated app verison

--- src/View/Home.jsx
+++ src/View/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
         <div className="pad"> 
             <Newsfeed/>
             <HomepageHeader/>
-            <a href="https://github.com/ibrahim0891/mvc-chat-app" className="text-center app-version dev-message tooltip"> App version: v3.09.24.1.37.1 
+            <a href="https://github.com/ibrahim0891/mvc-chat-app" className="text-center app-version dev-message tooltip"> App version: v3.16.24.4.48.1 
                 <span className="tooltiptext">View code</span>
             </a>
         </div>
